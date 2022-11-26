e3ee200d8f1f8c89f032f38a77b75041abc6ce09
Tolga BAYRAK <tolga@pop-os.localdomain>
2022-11-26 15:13:26 +0300

    nextjs, tailwindcss, chakra ui kurulumları

---
+++ client/pages/signup.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+function signup({}: Props) {
+  return (
+    <div>
+      <h1 className='text-xl m-auto'>Sign Up</h1>
+    </div>
+  )
+}
+
+export default signup
